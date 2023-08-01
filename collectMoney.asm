3e3da1947068968b04407f02caabb4d4e173efcd
Yatin Malhotra <yatin.a.malhotra@gmail.com>
2023-08-01 11:53:04 -0400

    Update

--- collectMoney.asm
+++ collectMoney.asm
@@ -30,109 +30,132 @@
 # #####################################################################
 
 .eqv BASE_ADDRESS 0x10008000
+.eqv GREEN 0x0ff00
+.eqv BROWN 0x964B00
+.eqv YELLOW 0x00ffff
+.eqv BLUE 0xADD8E6
 
 .text
-    li $t0, BASE_ADDRESS    # Set $t0 to BASE_ADDRESS
-    
-    move $a0, $t0 # Set a0 to BASE_ADDRESS (will be changed to the current position)
-    move $a1, $t0 # Set a1 to BASE_ADDRESS (will be changed to the new position)
-
-    # Call a function that creates the basic layout of the level
-
-    jal character
 
 main:
-    # Check for any keyboard input
-    li $t9, 0xffff0000
-    lw $t8, 0($t9)
-    beq $t8, 1, input           # if any input, go to the input function
-    j main                      # else go keep checking for any input
+	li $t1, BLUE 			# Center portion color
+	li $t2, BROWN			# Border color
+	j create_background		# Create the background
 
-input:
-    lw $t5, 4($t9)
-    beq $t5, 0x61, move_left    # if input is 'a', move left
-    beq $t5, 0x77, top_check     # if input is 'w', move up
-    beq $t5, 0x73, climb_down   # if input is 's', move down
-    beq $t5, 0x64, move_right   # if input is 'd', move right
-    beq $t5, 0x71, exit         # if input is 'p', restart the game (currently exiting)
+create_background:
+	move $a0, $gp			# Set $a0 to the place to write
+	li $a1, 65			# 65 pixels (full top row + one pixel on the left column)
+	move $a2, $t2			# BROWN
+	jal set_pixels			# set_pixels
+	li $t0, 62			# 62 rows of blue
+	j blue_rows_loop
 
-    j main                      # jump to main 	
+blue_rows_loop:
+	li $a1, 62
+	move $a2, $t1
+	jal set_pixels			# Set the 62 middle pixels to blue
+	j finish_border			# Finish the border after done
+	
+finish_border:
+	sw $t2, ($a0)
+	sw $t2, 4($a0)
+	addi $a0, $a0, 8
+	addi $t0, $t0, -1
+	bnez $t0, blue_rows_loop
+	
+	li $a1, 63
+	move $a2, $t2
+	jal set_pixels
+	
+	li $t0, BASE_ADDRESS    	# Set $t0 to BASE_ADDRESS
+    
+    	move $a0, $t0 			# Set a0 to BASE_ADDRESS (will be changed to the current position)
+    	move $a1, $t0 			# Set a1 to BASE_ADDRESS (will be changed to the new position)
+	
+	jal character
+	j input_checker
+
+set_pixels:
+	sw      $a2, ($a0)      	# set pixel
+    	addi    $a0, $a0, 4     	# advance memory pointer
+    	addi    $a1, $a1, -1    	# count-down loop
+    	bnez    $a1, set_pixels
+    	jr      $ra             	# return
+    
+input_checker:
+	li $t9, 0xffff0000
+    	lw $t8, 0($t9)
+    	beq $t8, 1, input_detected      # if any input, go to the input function
+    	j input_checker             	# else go keep checking for any input
+
+input_detected:
+	lw $t5, 4($t9)
+    	beq $t5, 0x61, move_left	# if input is 'a', move left
+    	beq $t5, 0x77, top_check	# if input is 'w', move up
+    	beq $t5, 0x73, climb_down	# if input is 's', move down
+    	beq $t5, 0x64, move_right	# if input is 'd', move right
+    	beq $t5, 0x71, exit		# if input is 'p', restart the game (currently exiting)
+
+	j input_checker
 
 top_check:
-    li $t2, BASE_ADDRESS	# Set $t2 to the BASE_ADDRESS
-    move $a0, $t0
-    addi $t0, $t0, -256         # Update the value of $t0 (moving left means -256 pixels)
-    
-    ble $t2, $t0, climb_up
-    bgt $t2, $t0, reset_top_val
+	li $t2, BASE_ADDRESS		# Set $t2 to the BASE_ADDRESS
+	move $a0, $t0
+	addi $t0, $t0, -256         	# Update the value of $t0 (moving left means -256 pixels)
+
+	ble $t2, $t0, climb_up
+	bgt $t2, $t0, reset_top_val
 
 reset_top_val:
-    addi $t0, $t0, 256
+	addi $t0, $t0, 256
     
-    jal character
+	jal character
     
-    j main
-
-bottom_check:
-    li $t2, BASE_ADDRESS
-    move $a0, $t0
-    addi $t0, $t0, 256
-    
-    addi $t2, $t2, 16380 # Set $t2 to the bottom right pixel
-    
-    ble $t2, $t0, climb_down
-    bgt $t2, $t0, reset_bottom_val
-
-reset_bottom_val:
-    addi $t0, $t0, -256
-    
-    jal character
-    
-    j main
+	j input_checker
 
 move_left:
-    move $a0, $t0               # Set $a0 to the current position
-    addi $t0, $t0, -4           # Update the value of $t0 (moving left means -4 pixels)
-    move $a1, $t0               # Set $a1 to $t0 (the new position)
+	move $a0, $t0               	# Set $a0 to the current position
+	addi $t0, $t0, -4           	# Update the value of $t0 (moving left means -4 pixels)
+	move $a1, $t0               	# Set $a1 to $t0 (the new position)
 
-    jal character
+	jal character
 
-    j main
+	j input_checker
 
 move_right:
-    move $a0, $t0               # Set $a0 to the current position
-    addi $t0, $t0, 4            # Update the value of $t0 (moving right means +4 pixels)
-    move $a1, $t0               # Set $a1 to $t0 (the new position)
+ 	move $a0, $t0               	# Set $a0 to the current position
+	addi $t0, $t0, 4            	# Update the value of $t0 (moving right means +4 pixels)
+	move $a1, $t0               	# Set $a1 to $t0 (the new position)
 
-    jal character
+	jal character
 
-    j main
+	j input_checker
 
 climb_up:
-    move $a1, $t0               # Set $a1 to $t0 (the new position)
+ 	move $a1, $t0               	# Set $a1 to $t0 (the new position)
 
-    jal character
+	jal character
 
-    j main
+	j input_checker
 
 climb_down:
-    move $a0, $t0               # Set $a0 to the current position
-    addi $t0, $t0, 256          # Update the value of $t0 (moving left means +256 pixels)
-    move $a1, $t0               # Set $a1 to $t0 (the new position)
+	move $a0, $t0               	# Set $a0 to the current position
+	addi $t0, $t0, 256          	# Update the value of $t0 (moving left means +256 pixels)
+	move $a1, $t0               	# Set $a1 to $t0 (the new position)
 
-    jal character
+	jal character
 
-    j main
+	j input_checker
 
 character:
-    li $t1, 0xffff00        # Set $t1 to YELLOW
-    li $t3, 0x000000        # Set $t7 to BLACK
+	li $t1, YELLOW			# Set $t1 to YELLOW
+    	li $t3, BLUE        		# Set $t7 to BLUE
 
-    sw $t3, 0($a0)          # Change the current pixel to BLACK
-    sw $t1, 0($a1)          # Change the newer pixel to the character
+    	sw $t3, 0($a0)          	# Change the current pixel to BLUE
+    	sw $t1, 0($a1)          	# Change the newer pixel to the character
 
-    jr $ra
+    	jr $ra
 
 exit:
-    li $v0, 10
-    syscall
+	li $v0, 10
+    	syscall
